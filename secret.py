bb88b87536819147084ba6683b922f2e6d29cbab
Aniket Mishra <ianiket23@gmail.com>
2021-09-24 15:59:27 -0600

    update

--- secret.py
+++ secret.py
@@ -11,9 +11,6 @@ class FollowingTheTAsInstructionsError(Exception):
             "and to delete this error!"
         ))
 
-# Delete this line:
-raise FollowingTheTAsInstructionsError
-
 # Edit the following two lines:
 username = "aniket"
 password = "c404"
