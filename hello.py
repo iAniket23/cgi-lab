9afe7ce42aac8e0c73fd1f87aaae719e301947f9
Aniket Mishra <ianiket23@gmail.com>
2021-09-24 21:11:36 -0600

    update

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+import secret, templates 
+from http import cookies
+import json
+import os
+
+# Q1
+print('Content-Type: application/json\r\n\r\n')
+print(json.dumps(dict(os.environ), indent = 4))
+
+# Q2
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<body>")
+message = "\nQuery string:{}".format(os.environ.get('QUERY_STRING'))
+print(message)
+print("<br>")
+message = "\nBrowser:{}".format(os.environ.get("HTTP_USER_AGENT"))
+print(message)
+print("<br>")
